23bb39094ebec6a746bdd63667070d77df1551f1
IGhost-Of-PastI <95056441+IGhost-Of-PastI@users.noreply.github.com>
2023-10-30 23:26:24 +0300

    Добавьте файлы проекта.

---
+++ PsyTests/OprosnicTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace PsyTests
+{
+    struct Keys
+    {
+        public string Question;
+        public string LinkedShkala;
+    }
+    internal class OprosnicTest
+    {
+        public OprosnicTest(string name, string opisanie, string pathToImage, string algorith, List<string> shakali, List<Keys> listOfKeys)
+        {
+            Name = name;
+            Opisanie = opisanie;
+            PathToImg = pathToImage;
+            Algorithm = algorith;
+            Shakli = shakali;
+            ListOfKeys = listOfKeys;
+        }
+        public string Name { get; set; }
+        public string Opisanie { get; set; }
+        public string PathToImg { get; set; }
+        public string Algorithm { get; set; }
+
+        List<string> Shakli;
+        List<Keys> ListOfKeys;
+
+
+        
+
+    }
+}
---
+++ PsyTests/WindowForTestTypes/OprosickType/TestsEditor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PsyTests
+{
+    /// <summary>
+    /// Логика взаимодействия для TestsEditor.xaml
+    /// </summary>
+    public partial class TestsEditor : Window
+    {
+        public TestsEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
